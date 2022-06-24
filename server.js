e354bf13e2aed588f7d17c5d34873bc00e6ce7a9
Lucas Fonseca <lucasrfon@gmail.com>
2022-06-24 13:39:42 -0300

    implementa GET /participants

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URL);
 let db;
-mongoClient.connect(() => {
+mongoClient.connect().then(() => {
   db = mongoClient.db("batePapo");
 });
 
@@ -14,6 +14,23 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+server.post('/participants', async (req, res) => {
+  const { name } = req.body
+  try {
+    const teste = await db.collection('participants').insertOne({ name, lastStatus: Date.now() });
+    res.status(201).send(teste);
+  } catch(error) {
+    res.sendStatus(422)
+  }
+});
 
+server.get('/participants', async (req, res) => {
+  try {
+    const participants = await db.collection('participants').find().toArray();
+    res.send(participants);
+  } catch(error) {
+    res.send("Algo de errado não está certo!")
+  }
+});
 
 server.listen(5000);
