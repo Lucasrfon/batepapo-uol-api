8be28c4bf92a7d6d29e4d08c3f024ea159e2d94d
Lucas Fonseca <lucasrfon@gmail.com>
2022-06-25 16:45:46 -0300

    implementa POST /participants

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
+import joi from 'joi';
+import dayjs from 'dayjs';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,17 +12,36 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("batePapo");
 });
 
+const participantSchema = joi.object({ name: joi.string().required() });
+
 const server = express();
 server.use(cors());
 server.use(express.json());
 
 server.post('/participants', async (req, res) => {
-  const { name } = req.body
+  const validation = participantSchema.validate(req.body);
+  const { name } = req.body;
+
+  if(validation.error) {
+    return res.status(422).send(validation.error.details[0].message);
+  };
+
+  if(await db.collection('participants').findOne({name: name})) {
+    return res.status(409).send("Nome em uso");
+  };
+
   try {
-    const teste = await db.collection('participants').insertOne({ name, lastStatus: Date.now() });
-    res.status(201).send(teste);
+    await db.collection('participants').insertOne({ name, lastStatus: Date.now() });
+    await db.collection('messages').insertOne({
+      from: name,
+      to: 'Todos',
+      text: 'entra na sala...',
+      type: 'status',
+      time: dayjs().format('HH:mm:ss')
+    })
+    res.status(201).send();
   } catch(error) {
-    res.sendStatus(422)
+    res.sendStatus(500);
   }
 });
 
@@ -33,4 +54,14 @@ server.get('/participants', async (req, res) => {
   }
 });
 
-server.listen(5000);
+server.delete('/participants', async (req, res) => {
+  const {name} = req.body
+  try {
+    const participants = await db.collection('participants').deleteOne({name: name});
+    res.send(participants);
+  } catch(error) {
+    res.send("Algo de errado não está certo!")
+  }
+})
+
+server.listen(5000, () => console.log("Server online."));
